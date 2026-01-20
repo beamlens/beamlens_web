24a4acd10e30d4aae71f7837e2cbbe86ff5ef406
Bradley Golden <golden.bradley@gmail.com>
2026-01-20 14:37:33 -0600

    Update test helper configuration

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,11 @@
 # Ensure Phoenix is started for tests
 Application.ensure_all_started(:phoenix)
 
+# Start the stores manually since BeamlensWeb is a library
+# that doesn't auto-start (no mod callback in application/0)
+{:ok, _} = BeamlensWeb.NotificationStore.start_link([])
+{:ok, _} = BeamlensWeb.InsightStore.start_link([])
+{:ok, _} = BeamlensWeb.EventStore.start_link([])
+
 # ExUnit configuration
 ExUnit.start()
