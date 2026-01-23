73d5aec67698eb3363c44d6c98ccee2e7ffc0037
Bradley Golden <golden.bradley@gmail.com>
2026-01-23 15:01:18 -0600

    Update beamlens to 0.3.0-pre (git ref 765c820)

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule BeamlensWeb.MixProject do
       {:bandit, "~> 1.0", only: :test},
       {:jason, "~> 1.4"},
       {:req, "~> 0.5"},
-      {:beamlens, "~> 0.2"},
+      {:beamlens, github: "beamlens/beamlens", ref: "765c820"},
       {:mdex, "~> 0.11"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
